6eab8d9a01f36197b83d2edff2d7663c7cc6f738
SreelekhaKadireddy <ksreelekha049@gmail.com>
2024-10-18 11:04:55 +0530

    job portal

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,29 @@ const morgan=require("morgan");
 const bodyparser=require("body-parser")
 require("dotenv").config();
 var cors=require("cors");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error")
 
 //database connection
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>console.log("DB connected"))
-.catch((err)=>console.log(err))
+.catch((err)=>console.log(err));
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyparser.json({limit:"5mb"}));
+app.use(bodyparser.urlencoded({
+    limit:"5mb",
+    extended:true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//error middleware
+app.use(errorHandler);
+
 //port
 const port=process.env.PORT || 8000;
 
